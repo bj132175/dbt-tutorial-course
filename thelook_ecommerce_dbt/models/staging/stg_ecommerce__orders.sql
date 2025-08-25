888c08ef16d634a24988aaacd39ed8d437561f3b
Jaskolski <bjasko01@atkearney.com>
2025-08-25 16:32:14 +0200

    create scr_ecommerce.yml, create stg_orders and to_timestamp macro

---
+++ thelook_ecommerce_dbt/models/staging/stg_ecommerce__orders.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+    SELECT *
+    FROM {{ source('raw', 'orders') }}
+)
+
+SELECT
+    order_id,
+    user_id,
+    status,
+    gender,
+    {{ string_to_timestamp('created_at') }} AS created_at,
+    {{ string_to_timestamp('returned_At') }} AS returned_At,
+    {{ string_to_timestamp('shipped_at') }} AS shipped_at,
+    {{ string_to_timestamp('delivered_at') }} AS delivered_at,
+    num_of_item
+FROM
+    source
